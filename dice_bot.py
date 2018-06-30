117d474ade758d69f1560e930559dce516b86264
GregHilston <Gregory.Hilston@gmail.com>
2018-06-29 19:21:58 -0600

    Woops got confused and put the dnd bot code here, adding the real dice bot code

--- dice_bot.py
+++ dice_bot.py
@@ -1,81 +1,41 @@
-import csv
-import os
-from logging.config import dictConfig
+import random
 from simple_slack_bot.simple_slack_bot import SimpleSlackBot
-
-PATH = os.path.dirname(os.path.realpath(__file__))
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-    'formatters': {
-        'verbose': {
-            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
-        },
-        'simple': {
-            'format': '%(levelname)s %(message)s'
-        },
-    },
-    'handlers': {
-        'console':{
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'formatter': 'simple'
-        },
-        'rotate': {
-            'level': 'INFO',
-            'formatter': 'verbose',
-            'class': 'logging.handlers.TimedRotatingFileHandler',
-            'filename': os.path.join(PATH, 'logs/dnd.log'),
-            'when': 'midnight',
-            'backupCount': 7,
-        }
-    },
-    'loggers': {
-        '': {
-            'handlers': ['console', 'rotate'],
-            'level': 'INFO'
-        }
-    }
-}
-dictConfig(LOGGING)
 
 simple_slack_bot = SimpleSlackBot(debug=True)
 
-users = {}
-
-def get_user(uid):
-    if uid not in users:
-        users[uid] = simple_slack_bot.helper_user_id_to_user_name(uid)
-    return users[uid]
 
 @simple_slack_bot.register("message")
 def roll_callback(request):
     """This function is called every time a message is sent to a channel out Bot is in
+
     :param request: the SlackRequest we receive along with the event. See the README.md for full documentation
     :return: None
     """
+    if request.message.lower().startswith("roll"):
+        dice_num_and_value = request.message.lower().split("roll")[1]
 
-    fields = []
+        try:
+            dice_num = int(dice_num_and_value.split("d")[0])
+            dice_value = int(dice_num_and_value.split("d")[1])
+        except ValueError:
+            request.write("Invalid format detected. Please call this bot using roll [int]d[int]")
+            return
 
-    if "CBGJ4P2JJ" != request.channel:
-        return
 
-    # player message
-    if request._slack_event.event.get("subtype") == "me_message":
-            fields.append("Player")
-            fields.append(get_user(request.user))
-            fields.append(request.message)
+        sum = 0
+        rolls = []
 
-    # DM message
-    elif request.message.startswith("&gt;"):
-            fields.append("DM")
-            fields.append(get_user(request.user))
-            fields.append(request.message)
+        if dice_num > 25:
+            request.write("We will not process more than 25 dice rolls, sorry @who_dat")
+            return
 
-    if fields:
-        with open(r"transcript.csv", 'a') as transcript:
-            writer = csv.writer(transcript)
-            writer.writerow(fields)
+        for die in range(dice_num):
+            roll = random.randint(1, dice_value)
+            sum += roll
+            rolls.append(roll)
+
+        request.write(f"Rolled {dice_num_and_value} and got {str(sum)} ({rolls})")
+
 
 def main():
     simple_slack_bot.start()
